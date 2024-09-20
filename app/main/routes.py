e21ef7a920507d006d9a4876bf23ac441758ac51
Mussorable <redduck5601@gmail.com>
2024-09-20 14:50:20 +0200

    added base and index pages

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, current_app
 
 from app.main import bp
 
@@ -7,5 +7,7 @@ from app.main import bp
 @bp.route('/index')
 def index():
     return render_template(
-        'index.html'
+        'index.html',
+        title='Home',
+        website_title=current_app.config['WEBSITE_TITLE'],
     )
