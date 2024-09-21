728c7e0835269d5136da5fc8893e280bcef2f5a1
Mussorable <redduck5601@gmail.com>
2024-09-21 11:46:49 +0200

    added markup, folium map, header

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,13 +1,17 @@
 from flask import render_template, current_app
 
 from app.main import bp
+from app.logic.map import generate_map
 
 
 @bp.route('/')
 @bp.route('/index')
 def index():
+    interactive_map = generate_map()
+
     return render_template(
         'index.html',
         title='Home',
         website_title=current_app.config['WEBSITE_TITLE'],
+        map=interactive_map,
     )
