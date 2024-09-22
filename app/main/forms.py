8f3de2d66a3bfdaabf912f8954b0f1a376089752
Mussorable <redduck5601@gmail.com>
2024-09-22 09:11:36 +0200

    added flask-login, index page login required

--- app/main/forms.py
+++ app/main/forms.py
@@ -9,7 +9,7 @@ from app.models import User
 
 
 class LoginForm(FlaskForm):
-    username = StringField('Username', validators=[DataRequired()])
+    email = StringField('Email', validators=[DataRequired()])
     password = PasswordField('Password', validators=[DataRequired()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Login')
--- app/main/routes.py
+++ app/main/routes.py
@@ -32,15 +32,17 @@ def login():
         return redirect(url_for('main.index'))
     form = LoginForm()
     if form.validate_on_submit():
-        user = db.session.scalar(sa.select(User).where(User.username == form.username.data))
+        user = db.session.scalar(sa.select(User).where(User.email == form.email.data))
         if user is None or not user.check_password(form.password.data):
             flash('Invalid username or password')
+            flash(f'{form.email.data} {form.password.data}')
             return redirect(url_for('main.login'))
         login_user(user, remember=form.remember_me.data)
         next_page = request.args.get('next')
         if not next_page or urlsplit(next_page).netloc != '':
             next_page = url_for('main.index')
         return redirect(next_page)
+
     return render_template(
         'login.html',
         title='Log In',
@@ -58,6 +60,19 @@ def logout():
 @bp.route('/register', methods=['GET', 'POST'])
 def register():
     form = RegistrationForm()
+    if current_user.is_authenticated:
+        return redirect(url_for('main.index'))
+    if form.validate_on_submit():
+        user = User(
+            username=form.username.data,
+            email=form.email.data
+        )
+        user.set_password(form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Congratulations, you are now a registered user!')
+        return redirect(url_for('main.login'))
+
     return render_template(
         'register.html',
         title='Register',
